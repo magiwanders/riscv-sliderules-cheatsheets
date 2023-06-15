5a1aab51c3c20731f714b13299568a8d5ac6f2c4
magiwanders <simone.shawn.cazzaniga@gmail.com>
2023-06-15 14:33:24 +0200

    Added ash-html

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('RISC-V Sliderules Cheatsheets Loaded!')
     sliderules.loadInto('container')
+    document.getElementById('tester').appendChild(
+      _div({id: 'simulation_controls', style: 'height:500px; width:100%; pointer-events:painted;'},
+        [
+            _button({id: 'toggle_simulation'}, 'Pause'), 'or',
+            _button({id: 'step'}, 'Step'), ' the simulation.',
+            _br(), _br()
+        ]
+      )
+    )
   }
 )
