05a372f1680084cf2c9388a759c92447cafc4620
Jayaraj J <jayarajevur@gmail.com>
2023-06-24 14:13:58 +0530

    added modified data strucutre generator for RV32I

---
+++ experiments/datastructure/generate_instr.js
@@ -0,0 +1,102 @@
+import fs from 'fs';
+
+// Input data structure
+const instructions = {
+  ISA: "RV",
+  funct3Mask: 0b00000000000000000111000000000000, // Common funct3 mask
+  funct7Mask: 0b11111110000000000000000000000000, // Common funct7 mask
+  instructions: [
+    {
+      name: "add",
+      assembly: "rd, rs1, rs2",
+      description: "Addition",
+      type: "R",
+      quadrant: 1,
+      arch_width: 32,
+      extension: "I",
+      opcode: {
+        value: 0b0110011,
+        length: 7
+      },
+      funct3: {
+        value: 0b000,
+        mask: 0b11100
+      },
+      funct7: {
+        value: 0b0000000,
+        mask: 0b1111111
+      },
+      fields: [
+        {
+          name: "rd",
+          position: 7,
+          length: 5
+        },
+        {
+          name: "rs1",
+          position: 15,
+          length: 5
+        },
+        {
+          name: "rs2",
+          position: 20,
+          length: 5
+        }
+      ]
+    },
+    {
+      name: "sub",
+      assembly: "rd, rs1, rs2",
+      description: "Subtraction",
+      type: "R",
+      quadrant: 1,
+      arch_width: 32,
+      extension: "I",
+      opcode: {
+        value: 0b0110011,
+        length: 7
+      },
+      funct3: {
+        value: 0b000,
+        mask: 0b11100
+      },
+      funct7: {
+        value: 0b0000000,
+        mask: 0b1111111
+      },
+      fields: [
+        {
+          name: "rd",
+          position: 7,
+          length: 5
+        },
+        {
+          name: "rs1",
+          position: 15,
+          length: 5
+        },
+        {
+          name: "rs2",
+          position: 20,
+          length: 5
+        }
+      ]
+    }
+
+  ]
+};
+
+// Convert the data structure to JSON format
+const jsonData = JSON.stringify(instructions, null, 2);
+
+// Write the JSON data to a file
+fs.writeFileSync('isa/riscv32/instructions.json', jsonData);
+
+const parsedInstructions = JSON.parse(jsonData);
+
+// Example: Extracting the funct3 field using the mask
+const instruction = parsedInstructions.instructions[0];
+const funct3Mask = instruction.funct3.mask;
+const funct3Value = (instruction.opcode.value & funct3Mask) >>> funct3Mask.toString(2).length;
+
+console.log(`funct3 Value: ${funct3Value.toString(2)}`); // Output: funct3 Value: 0
---
+++ experiments/datastructure/testds1.js
@@ -0,0 +1,211 @@
+
+
+const instructions = {
+    add: {
+        name: "add",
+        mnemonics: "add rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b000 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0000000 }
+        ],
+        pseudocode: `
+            rd = rs1 + rs2;
+            `
+    },
+    sub: {
+        name: "sub",
+        mnemonics: "sub rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b000 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0100000 }
+        ],
+        pseudocode: `
+            rd = rs1 - rs2;
+            `
+    },
+    xor: {
+        name: "xor",
+        mnemonics: "xor rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b100 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0000000 }
+        ],
+        pseudocode: `
+            rd = rs1 ^ rs2;
+            `
+    },
+    slt: {
+        name: "slt",
+        mnemonics: "slt rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b010 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0000000 }
+        ],
+        pseudocode: `
+            rd = {   31'b0,    ( rs1[31] == rs2[31] )  ?  (rs1<rs2)  :  (rs1[31])   };
+            `
+    },
+    or: {
+        name: "or",
+        mnemonics: "or rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b110 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0000000 }
+        ],
+        pseudocode: `
+            rd = rs1 | rs2;
+            `
+    },
+    sll: {
+        name: "sll",
+        mnemonics: "sll rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b001 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0000000 }
+        ],
+        pseudocode: `
+            rd = rs1  <<	zero extends	rs2[4:0] ;
+            `
+    },
+    srl: {
+        name: "srl",
+        mnemonics: "srl rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b101 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0000000 }
+        ],
+        pseudocode: `
+            rd = rs1	zero extends	>>	rs2[4:0] ;
+            `
+    },
+    sra: {
+        name: "sra",
+        mnemonics: "sra rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b101 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0100000 }
+        ],
+        pseudocode: `
+            rd = rs1	msb extends	>>	rs2[4:0] ;
+            `
+    },
+    sltu: {
+        name: "sltu",
+        mnemonics: "sltu rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b011 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0000000 }
+        ],
+        pseudocode: `
+            rd =  { 31'b0, rs1 < rs2 }	;
+            `
+    },
+    and: {
+        name: "and",
+        mnemonics: "and rd, rs1, rs2",
+        fields: [
+            { name: "opcode", bits: 0b0110011 },
+            { name: "rd", bits: "5" },
+            { name: "funct3", bits: 0b111 },
+            { name: "rs1", bits: "5" },
+            { name: "rs2", bits: "5" },
+            { name: "funct7", bits: 0b0000000 }
+        ],
+        pseudocode: `
+            rd = rs1  &	rs2 ;
+            `
+    },
+
+};
+
+
+// Function to encode an instruction
+function encodeInstruction({ instruction }) {
+    const parts = instruction.split(" ");
+    const opcode = instructions[parts[0]].fields.find(field => field.name === "opcode").bits;
+    const rd = extractRegisterNumber({ register: parts[1] });
+    const rs1 = extractRegisterNumber({ register: parts[2] });
+    const rs2 = extractRegisterNumber({ register: parts[3] });
+
+    let encodedInstruction = 0;
+    encodedInstruction |= opcode << 0;
+    encodedInstruction |= rd << 7;
+    encodedInstruction |= rs1 << 15;
+    encodedInstruction |= rs2 << 20;
+
+    return encodedInstruction;
+}
+
+// Function to extract register number from a string like "x7"
+function extractRegisterNumber({ register }) {
+    return parseInt(register.substr(1));
+}
+
+// Function to decode a 32-bit value into an instruction
+function decodeInstruction({ value }) {
+    for (const instruction in instructions) {
+        const fields = instructions[instruction].fields;
+        const opcode = fields.find(field => field.name === "opcode").bits;
+        const rd = (value >> 7) & 0b11111;
+        const rs1 = (value >> 15) & 0b11111;
+        const rs2 = (value >> 20) & 0b11111;
+
+        if (opcode === (value & 0b1111111)) {
+            return {
+                instruction,
+                decodedInstruction: {
+                    rd,
+                    rs1,
+                    rs2
+                }
+            };
+        }
+    }
+
+    return null;
+}
+
+// Example usage
+const encoded = encodeInstruction({ instruction: "add rd, x7, x11" });
+console.log("Encoded instruction:", encoded.toString(2));
+console.log("Encoded instruction:", encoded);
+
+
+const decoded = decodeInstruction({ value: encoded });
+console.log("Decoded instruction:", decoded);
