8ebcaf9269ad9594edf7e99db23b8bfad4e070dc
magiwanders <50774142+magiwanders@users.noreply.github.com>
2023-07-07 00:53:05 +0200

    Update helper-functions.mjs

--- src/isa/helper-functions.mjs
+++ src/isa/helper-functions.mjs
@@ -16,6 +16,70 @@ export function pruneInstructions({ constraints: {} }) {
     let pruneInstructions = {}
 }
 
+// Makes the pruned instruction into a data structure most similar to a sliderules row
+export function tabulateInstructionEncode({ prunedInstruction: {}}) {
+    /*
+    Example of tabulation:
+    
+    input:    
+    
+    add: {
+      ISA: "RV",
+      assembly: ["rd", "rs1", "rs2"],
+      pseudocode: 'rd = rs1 + rs2',
+      description: "Addition",
+      type: "R",
+      arch_width: [32, 64],
+      extension: "I",
+      fields: {
+        opcode: { value: 0b0110011, mask: 0b1111111 },
+        funct3: { value: 0b000, mask: 0b111 << 12 },
+        funct7: { value: 0b0000000, mask: 0b1111111 << 25 },
+        rs1: { mask: 0b11111 << 15 },
+        rs2: { mask: 0b11111 << 20 },
+        rd: { mask: 0b11111 << 7 }
+      }
+    }
+
+    output:
+
+    [
+        {
+            content: 'I',
+            width: 1,
+        },
+        {
+            content: 'R',
+            width: 1,
+        },
+        {
+            content: 'Addition',
+            width: 1,
+        },
+        ...
+        {
+            content: 'funct7',
+            width: 7,
+            value: 0b0000000
+        },
+        {
+            content: 'rs2',
+            width: 5,
+        },
+        {
+            content: 'rs1',
+            width: 5,
+        },
+        ...
+        {
+            content: 'rd = rs1 + rs2'
+            width: 1,
+        },
+        ...
+    ]
+    */
+}
+
 // Input: a mask in binary form. Output: number of ones in the mask (its width).
 export function maskWidth({mask: 0b0}) {
 
